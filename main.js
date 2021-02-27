830d59ee284f5d701b44d0c1b6a0f9d06ceaec6b
YYCKING1 <75652154+YYCKING1@users.noreply.github.com>
2021-02-27 12:36:13 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log( "My Project C77");
+var name="Ahmad Tabbal";
+var math=Math.max(1,15);
+console.log(math)
+var mather=Math.min(10,5);
+console.log(mather)
+var mathist=Math.sqrt(100);
+console.log(mathist)
+var mathter=Math.sqrt(4);
+console.log(mathter)
